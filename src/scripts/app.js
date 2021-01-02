e5116a6d2c95e3324c4350b5ee16d78c8e118cd5
Caleb Wardlaw <calebwardlaw@gmail.com>
2021-01-01 19:29:24 -0600

    added index.html and app.js

---
+++ src/scripts/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+console.log('App.js is running!');
+
+// JSX - JavaScript XML
+// var template = <p>This is JSX from app.js!</p>;
+var template = React.createElement(
+  "h1",
+  { id: "someid" },
+  "Something new"
+);
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
