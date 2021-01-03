a4dce7b04df440151ab1143effcbee31b5c4b5bf
Caleb Wardlaw <calebwardlaw@gmail.com>
2021-01-02 18:30:21 -0600

    accesed object values

--- src/index.js
+++ src/index.js
@@ -1,29 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
-console.log('App.js is running!');
-
-// JSX - JavaScript XML
-// var template = <p>This is JSX from app.js!</p>;
-
-
-
 class Indecision extends React.Component {
     render() {
-        return (
-            <div className="hello">
-                <h1>HELLO</h1>
+
+
+        //Class variables
+        // let template = (
+        //     <div className="hello">
+        //         <h1>Hello World!</h1>
+        //         <ol>
+        //             <li>Item one</li>
+        //             <li>Item two</li>
+        //         </ol>
+        //     </div>
+        // )
+        let user = {
+            name: 'Caleb',
+            age: 29,
+            location: 'San Antonio'
+        }
+
+        let app = {
+            title: 'Indecision App',
+            subtitle: 'Choosing a Path'
+
+
+    }
+        let templateTwo = (
+            <div className="challenge">
+                {/*Braces for vars*/}
+                <h1>{user.name}</h1>
+                <h2>{app.title}</h2>
+                <h3>{app.subtitle}</h3>
+                <p>Age: {user.age}</p>
+                <p>Location: {user.location}</p>
             </div>
+        );
+        return (
+            templateTwo
+
         );
     }
 }
-
-
-
-
-
-
 
 ReactDOM.render(
     <Indecision />,
